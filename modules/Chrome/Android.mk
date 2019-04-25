3892cb8391fc1f01cbc6f9bff4fcd82f4fdfad49
hairycheez <22867901+hairycheez@users.noreply.github.com>
2019-04-25 09:03:37 -0400

    Update Android.mk

--- modules/Chrome/Android.mk
+++ modules/Chrome/Android.mk
@@ -8,6 +8,6 @@ GAPPS_LOCAL_OVERRIDES_MIN_VARIANT := stock
 GAPPS_LOCAL_OVERRIDES_PACKAGES := webview Browser Browser2 BrowserProviderProxy Chromium Fluxion Gello Jelly
 
 LOCAL_REQUIRED_MODULES := libwebviewchromium_loader \
-                          libwebviewchromium_plat_support                          
+                          libwebviewchromium_plat_support
 
 include $(BUILD_GAPPS_PREBUILT_APK)
